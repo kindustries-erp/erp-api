babf7e0db92d4e1c4a39e6fa3846c31b272dd4ca
Liouni <dev@liouni.com>
2026-05-15 18:29:21 +0700

    Wire Viettel v2 module into sinvoice surface

--- src/sinvoice/sinvoice.module.ts
+++ src/sinvoice/sinvoice.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SinvoiceController } from './sinvoice.controller';
 import { SinvoiceService } from './sinvoice.service';
+import { ViettelV2Module } from '../viettel-v2/viettel-v2.module';
 
 @Module({
+  imports: [ViettelV2Module],
   controllers: [SinvoiceController],
   providers: [SinvoiceService],
   exports: [SinvoiceService],
