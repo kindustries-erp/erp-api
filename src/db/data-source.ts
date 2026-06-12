286aa45083ce1d35f4ae7fd6bc0c89255e52442b
Liouni <dev@liouni.com>
2026-06-12 11:12:19 +0700

    fix(db): correct migration config and reduce drop constraints in isDeleted migration

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -52,7 +52,7 @@ export default new DataSource(
         type: 'postgres',
         url: databaseUrl,
         entities,
-        migrations: [__dirname + '/../migrations/**/*.js'],
+        migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
         synchronize: false,
         ssl: { rejectUnauthorized: false },
       }
@@ -64,7 +64,7 @@ export default new DataSource(
         password: process.env.DB_PASSWORD || '',
         database: process.env.DB_DATABASE || 'erp_core',
         entities,
-        migrations: [__dirname + '/../migrations/**/*.js'],
+        migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
         synchronize: false,
         ssl:
           process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
