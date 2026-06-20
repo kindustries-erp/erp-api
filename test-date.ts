c12ec79c46d5bf5e29ad0ce74f6e8b1b6dc7a59c
Liouni <dev@liouni.com>
2026-06-20 12:47:08 +0700

    hotfix: remove rogue test-date.ts breaking production build output path

--- test-date.ts
+++
@@ -1,4 +0,0 @@
-import { DataSource, Between } from 'typeorm';
-import { ErpProductionOrder } from './src/production-core/entities/erp_production_order.entity';
-
-console.log(Between('2026-06-01', '2026-06-30'));
