8389c3cfaa1fc7cccd152225f789007dc53ed5ed
Liouni <dev@liouni.com>
2026-07-05 22:22:08 +0700

    feat: enhance public warranty activation logic

--- src/public-warranty/dto/check-warranty.dto.ts
+++ src/public-warranty/dto/check-warranty.dto.ts
@@ -3,9 +3,9 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CheckWarrantyDto {
   @IsString()
   @IsNotEmpty()
-  sokhung: string;
+  vin_no: string;
 
   @IsString()
   @IsNotEmpty()
-  somay: string;
+  engine_no: string;
 }
