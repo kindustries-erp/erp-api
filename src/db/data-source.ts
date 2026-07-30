08f3f070a1174376fdcd04a984d5be104b7f2001
Liouni <dev@liouni.com>
2026-07-30 14:20:45 +0700

    feat: add module column to attachments and fix bugs

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -141,26 +141,26 @@ const databaseUrl = process.env.DATABASE_URL;
 export default new DataSource(
   databaseUrl
     ? {
-        type: 'postgres',
-        url: databaseUrl,
-        schema: 'public',
-        entities,
-        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-        synchronize: false,
-        ssl: { rejectUnauthorized: false },
-      }
+      type: 'postgres',
+      url: databaseUrl,
+      schema: 'public',
+      entities,
+      migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+      synchronize: false,
+      ssl: { rejectUnauthorized: false },
+    }
     : {
-        type: 'postgres',
-        host: process.env.DB_HOST || '127.0.0.1',
-        port: Number(process.env.DB_PORT || 5432),
-        username: process.env.DB_USER || 'postgres',
-        password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_DATABASE || 'erp_core',
-        schema: 'public',
-        entities,
-        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-        synchronize: false,
-        ssl:
-          process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-      },
+      type: 'postgres',
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: Number(process.env.DB_PORT || 5432),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || 'erp_core',
+      schema: 'public',
+      entities,
+      migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+      synchronize: false,
+      ssl:
+        process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    },
 );
